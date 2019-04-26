1caf20e081cae0448eed7bce809bed00e6b4b731
Paweł Piekutowski <futureiscode@gmail.com>
2019-04-26 15:07:02 +0200

    Komentarze, skróty klawiszowe & snipety, rzutowanie zmiennych, wczytywanie danych z konsoli,

--- Lekcja5/Program.cs
+++ Lekcja5/Program.cs
@@ -10,6 +10,55 @@ namespace Lekcja5
     {
         static void Main(string[] args)
         {
+            // KOMENTARZE
+
+            // Test Test
+
+            /*
+             *Test 
+             * Test
+             */
+
+            // SKRÓTY KLAWISZOWE & SNIPETY
+            /*
+             * Układanie kodu ctrl k ctrl d
+             *
+             * Komentowanie kodu Ctrl+ e c
+             * Od komentowanie kodu Ctrl+ e u
+             *
+             * cw + 2x Tab Console.WriteLine();
+             * 
+             * 
+             */
+
+            //RZUTOWANIE ZMIENNYCH
+
+            //int i = int.MaxValue;
+            //short s = short.MaxValue;
+
+            //s = (short)i;
+            //Console.WriteLine(s);
+
+            //WCZYTYWANIE DANYCH Z KONSOLI
+
+                //Wczytywanie całej lini z konsoli jaką wprowadził użytkownik  
+                    //Console.ReadLine();
+                    //string s = Console.ReadLine();
+                    //Console.WriteLine(s);
+
+                //Wczytywanie jaki przycisk został wciśnięty  
+                    //Console.ReadKey();
+                    //if(Console.ReadKey().Key == ConsoleKey.D1)
+                    //Console.WriteLine("PRAWDA");
+            
+            
+
+
+
+
+
+
+            Console.ReadKey();
         }
     }
 }
