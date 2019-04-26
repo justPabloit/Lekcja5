1ae03201edc4f4ca768b2d0e78f1ab42e88d46c6
Paweł Piekutowski <futureiscode@gmail.com>
2019-04-26 15:34:27 +0200

    Konwersja typów (string>int, int>string),łączenie tekstu, formatowanie, mdyfikacja konsoli.

--- Lekcja5/Program.cs
+++ Lekcja5/Program.cs
@@ -20,6 +20,7 @@ namespace Lekcja5
              */
 
             // SKRÓTY KLAWISZOWE & SNIPETY
+
             /*
              * Układanie kodu ctrl k ctrl d
              *
@@ -33,27 +34,47 @@ namespace Lekcja5
 
             //RZUTOWANIE ZMIENNYCH
 
-            //int i = int.MaxValue;
-            //short s = short.MaxValue;
+            int i = int.MaxValue;
+            short s = short.MaxValue;
 
-            //s = (short)i;
-            //Console.WriteLine(s);
+            s = (short)i;
+            Console.WriteLine(s);
 
             //WCZYTYWANIE DANYCH Z KONSOLI
 
-                //Wczytywanie całej lini z konsoli jaką wprowadził użytkownik  
-                    //Console.ReadLine();
-                    //string s = Console.ReadLine();
-                    //Console.WriteLine(s);
+            //Wczytywanie całej lini z konsoli jaką wprowadził użytkownik  
+            Console.ReadLine();
+            string k = Console.ReadLine();
+            Console.WriteLine(k);
 
-                //Wczytywanie jaki przycisk został wciśnięty  
-                    //Console.ReadKey();
-                    //if(Console.ReadKey().Key == ConsoleKey.D1)
-                    //Console.WriteLine("PRAWDA");
-            
-            
+            //Wczytywanie jaki przycisk został wciśnięty  
+            Console.ReadKey();
+            if (Console.ReadKey().Key == ConsoleKey.D1)
+                Console.WriteLine("PRAWDA");
 
+            //KONWERSJA TYPÓW (STRING > INT, INT >STRING )
 
+            string c = Console.ReadLine();
+            int d = int.Parse(c) + 2;
+            Console.WriteLine(i);
+
+            c = d.ToString();
+            Console.WriteLine(c);
+
+            //ŁĄCZENIE TEKSTU & FORMATOWANIE "WRITELINE"
+
+            string s1 = "Witaj";
+            string s2 = "Świecie";
+            Console.WriteLine(s1 + s2 + "!!!{0} \n {1}", s1, s2);
+
+            //MODYFIKACJA KONSOLI
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.BackgroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine(s1 + s2 + "!!!{0} \n {1}", s1, s2);
+            Console.ResetColor();
+            Console.WriteLine(s1 + s2 + "!!!{0} \n {1}", s1, s2);
+            Console.ReadKey();
+            Console.Clear();
 
 
 
